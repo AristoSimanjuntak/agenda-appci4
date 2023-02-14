cb7815c846d3c6db956cc87b1c0af24617c80eb6
Aristo Simanjuntak <aristosimanjuntak94@gmail.com>
2023-02-14 10:11:29 +0700

    memperbaiki header menggunakan base url

--- app/Views/Layouts/header.php
+++ app/Views/Layouts/header.php
@@ -10,31 +10,31 @@
     <meta name="keywords" content="admin template, admin dashboard, bootstrap dashboard template, bootstrap 4 admin template, codeigniter 4 admin panel, template codeigniter bootstrap, php, codeigniter, php framework, web template, html5 template, php code, php html, codeigniter 4, codeigniter mvc">
 
     <!-- Favicon -->
-    <link rel="icon" href="http://localhost/appci4/public/img/logo.png" type="image/x-icon" />
+    <link rel="icon" href="<?= base_url('/img/logo.png') ?>" type="image/x-icon" />
 
     <!-- Title -->
     <title><?= $title ?? 'Dashboard' ?></title>
 
     <!-- Bootstrap css-->
-    <link href="http://localhost/appci4/public/assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="<?= base_url('/assets/plugins/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet" />
 
     <!-- Icons css-->
-    <link href="http://localhost/appci4/public/assets/plugins/web-fonts/icons.css" rel="stylesheet" />
-    <link href="http://localhost/appci4/public/assets/plugins/web-fonts/font-awesome/font-awesome.min.css" rel="stylesheet" />
-    <link href="/assets/plugins/web-fonts/plugin.css" rel="stylesheet" />
+    <link href="<?= base_url('/assets/plugins/web-fonts/icons.css') ?>" rel="stylesheet" />
+    <link href="<?= base_url('/assets/plugins/web-fonts/font-awesome/font-awesome.min.css') ?>" rel="stylesheet" />
+    <link href="<?= base_url('/assets/plugins/web-fonts/plugin.css') ?>" rel="stylesheet" />
 
     <!-- Style css-->
-    <link href="http://localhost/appci4/public/assets/css/style.css" rel="stylesheet" />
-    <link href="http://localhost/appci4/public/assets/css/skins.css" rel="stylesheet" />
-    <link href="http://localhost/appci4/public/assets/css/dark-style.css" rel="stylesheet" />
-    <link href="http://localhost/appci4/public/assets/css/colors/default.css" rel="stylesheet" />
+    <link href="<?= base_url('/assets/css/style.css') ?>" rel="stylesheet" />
+    <link href="<?= base_url('/assets/css/skins.css') ?>" rel="stylesheet" />
+    <link href="<?= base_url('/assets/css/dark-style.css') ?>" rel="stylesheet" />
+    <link href="<?= base_url('/assets/css/colors/default.css') ?>" rel="stylesheet" />
 
     <!-- Color css-->
-    <link id="theme" rel="stylesheet" type="text/css" media="all" href="http://localhost/appci4/public/assets/css/colors/color.css" />
+    <link id="theme" rel="stylesheet" type="text/css" media="all" href="<?= base_url('/assets/css/colors/color.css') ?>" />
 
     <!-- Select2 css-->
-    <link href="http://localhost/appci4/public/assets/plugins/select2/css/select2.min.css" rel="stylesheet">
+    <link href="<?= base_url('/assets/plugins/select2/css/select2.min.css') ?>" rel="stylesheet">
 
     <!-- Sidemenu css-->
-    <link href="http://localhost/appci4/public/assets/css/sidemenu/sidemenu.css" rel="stylesheet">
+    <link href="<?= base_url('/assets/css/sidemenu/sidemenu.css" rel="stylesheet') ?>">
 </head>
