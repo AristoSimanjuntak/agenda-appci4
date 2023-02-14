6163b0c8e835afc3cf34bff395f6610667f832a2
Aristo Simanjuntak <aristosimanjuntak94@gmail.com>
2023-02-14 10:14:43 +0700

    memperbaiki link js dengan base url

--- app/Views/Layouts/javascrip.php
+++ app/Views/Layouts/javascrip.php
@@ -3,79 +3,79 @@
 
 
 <!-- Jquery js-->
-<script src="http://localhost/appci4/public/assets/plugins/jquery/jquery.min.js"></script>
+<script src="<?= base_url('/assets/plugins/jquery/jquery.min.js'); ?> "></script>
 
 <!-- Bootstrap js-->
-<script src="http://localhost/appci4/public/assets/plugins/bootstrap/js/popper.min.js"></script>
-<script src="http://localhost/appci4/public/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src=" <?= base_url('/assets/plugins/bootstrap/js/popper.min.js'); ?> "></script>
+<script src="<?= base_url('/assets/plugins/bootstrap/js/bootstrap.min.js'); ?> "></script>
 
 <!-- Perfect-scrollbar js -->
-<script src="http://localhost/appci4/public/assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+<script src=" <?= base_url('/assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js'); ?> "></script>
 
 <!-- Sidemenu js -->
-<script src="http://localhost/appci4/public/assets/plugins/sidemenu/sidemenu.js"></script>
+<script src="<?= base_url('/assets/plugins/sidemenu/sidemenu.js'); ?> "></script>
 
 <!-- Sidebar js -->
-<script src="http://localhost/appci4/public/assets/plugins/sidebar/sidebar.js"></script>
+<script src=" <?= base_url('/assets/plugins/sidebar/sidebar.js'); ?> "></script>
 
 <!-- Select2 js-->
-<script src="http://localhost/appci4/public/assets/plugins/select2/js/select2.min.js"></script>
+<script src="<?= base_url('/assets/plugins/select2/js/select2.min.js'); ?> "></script>
 
 <!-- Sticky js -->
-<script src="http://localhost/appci4/public/assets/js/sticky.js"></script>
+<script src=" <?= base_url('/assets/js/sticky.js'); ?> "></script>
 
 <!-- Custom js -->
-<script src="http://localhost/appci4/public/assets/js/custom.js"></script>
+<script src="<?= base_url('/assets/js/custom.js'); ?> "></script>
 
 <!-- Internal Chart.Bundle js-->
-<script src="http://localhost/appci4/public/assets/plugins/chart.js/Chart.bundle.min.js"></script>
+<script src=<?= base_url('/assets/plugins/chart.js/Chart.bundle.min.js'); ?> "></script>
 
 <!-- Peity js-->
-<script src="http://localhost/appci4/public/assets/plugins/peity/jquery.peity.min.js"></script>
+<script src=" <?= base_url('/assets/plugins/peity/jquery.peity.min.js'); ?> " ></script>
 
 <!-- Internal Morris js -->
-<script src="http://localhost/appci4/public/assets/plugins/raphael/raphael.min.js"></script>
-<script src="http://localhost/appci4/public/assets/plugins/morris.js/morris.min.js"></script>
+<script src=" <?= base_url('/assets/plugins/raphael/raphael.min.js'); ?> "></script>
+<script src=" <?= base_url('/assets/plugins/morris.js/morris.min.js'); ?> "></script>
 
 <!-- Circle Progress js-->
-<script src="http://localhost/appci4/public/assets/js/circle-progress.min.js"></script>
-<script src="http://localhost/appci4/public/assets/js/chart-circle.js"></script>
+<script src=" <?= base_url('/assets/js/circle-progress.min.js'); ?> "></script>
+<script src=" <?= base_url('/assets/js/chart-circle.js') ?> " ></script>
 
 <!-- Internal Dashboard js-->
-<script src="http://localhost/appci4/public/assets/js/index.js"></script>
+<script src=" <?= base_url('/assets/js/index.js'); ?> "></script>
 
 <!-- Internal Datepicker js -->
-<script src="/assets/plugins/model-datepicker/js/datepicker.js"></script>
-<script src="/assets/plugins/model-datepicker/js/main.js"></script>
+<script src=<?= base_url('/assets/plugins/model-datepicker/js/datepicker.js'); ?> "></script>
+<script src=<?= base_url('/assets/plugins/model-datepicker/js/main.js'); ?> "></script>
 
 <!-- Modal js-->
-<script src="http://localhost/appci4/public/assets/js/modal.js"></script>
+<script src=" <?= base_url('/assets/js/modal.js'); ?> " ></script>
 
 <!-- Internal Jquery-Ui js-->
-<script src="http://localhost/appci4/public/assets/plugins/jquery-ui/ui/widgets/datepicker.js"></script>
+<script src=" <?= base_url('/assets/plugins/jquery-ui/ui/widgets/datepicker.js'); ?> "></script>
 
 <!-- Internal Jquery.maskedinput js-->
-<script src="http://localhost/appci4/public/assets/plugins/jquery.maskedinput/jquery.maskedinput.js"></script>
+<script src=" <?= base_url('/assets/plugins/jquery.maskedinput/jquery.maskedinput.js'); ?> " ></script>
 
 <!-- Internal Specturm-colorpicker js-->
-<script src="http://localhost/appci4/public/assets/plugins/spectrum-colorpicker/spectrum.js"></script>
+<script src=" <?= base_url('/assets/plugins/spectrum-colorpicker/spectrum.js'); ?> "></script>
 
 <!-- Internal Ion-rangeslider js-->
-<script src="http://localhost/appci4/public/assets/plugins/ion-rangeslider/js/ion.rangeSlider.min.js"></script>
+<script src=" <?= base_url('/assets/plugins/ion-rangeslider/js/ion.rangeSlider.min.js'); ?> " ></script>
 
 <!-- Select2 js-->
-<script src="http://localhost/appci4/public/assets/js/select2.js"></script>
+<script src=" <?= base_url('/assets/js/select2.js'); ?> "></script>
 
 <!--Bootstrap-datepicker js-->
-<script src="http://localhost/appci4/public/assets/plugins/bootstrap-datepicker/bootstrap-datepicker.js"></script>
+<script src=" <?= base_url('/assets/plugins/bootstrap-datepicker/bootstrap-datepicker.js'); ?> " ></script>
 
 <!-- Internal jquery-simple-datetimepicker js -->
-<script src="http://localhost/appci4/public/assets/plugins/amazeui-datetimepicker/js/amazeui.datetimepicker.min.js"></script>
+<script src=" <?= base_url('/assets/plugins/amazeui-datetimepicker/js/amazeui.datetimepicker.min.js'); ?> "></script>
 
 <!-- Internal Form-elements js-->
-<script src="http://localhost/appci4/public/assets/js/form-elements.js"></script>
+<script src=" <?= base_url('/assets/js/form-elements.js'); ?> " ></script>
 
-<script src="<?php echo  base_url('assets/plugins/datatable/jquery.dataTables.min.js'); ?>"></script>
+<script src=" <?php echo  base_url('assets/plugins/datatable/jquery.dataTables.min.js'); ?>"></script>
 <script src="<?php echo  base_url('assets/plugins/datatable/dataTables.bootstrap4.min.js'); ?>"></script>
 <script src="<?php echo  base_url('assets/plugins/datatable/dataTables.responsive.min.js'); ?>"></script>
 <script src="<?php echo  base_url('assets/plugins/datatable/fileexport/dataTables.buttons.min.js'); ?>"></script>
