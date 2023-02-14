54fd7b6b56b7cc8cc6280182d3d84a98ea0ad4be
Aristo Simanjuntak <aristosimanjuntak94@gmail.com>
2023-02-14 10:49:16 +0700

    mengubah link menggunakan base url

--- app/Views/Auth/indexView.php
+++ app/Views/Auth/indexView.php
@@ -9,36 +9,36 @@
     <meta name="keywords" content="admin template, admin dashboard, bootstrap dashboard template, bootstrap 4 admin template, codeigniter 4 admin panel, template codeigniter bootstrap, php, codeigniter, php framework, web template, html5 template, php code, php html, codeigniter 4, codeigniter mvc">
 
     <!-- Favicon -->
-    <link rel="icon" href="http://localhost/appci4/public/img/logo.png" type="image/x-icon" />
+    <link rel="icon" href="<?= base_url('/img/logo.png') ?>" type="image/x-icon" />
 
     <!-- Title -->
     <title><?= $title ?? 'Dashboard' ?></title>
 
     <!-- Bootstrap css-->
-    <link href="http://localhost/appci4/public/assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="<?= base_url('/assets/plugins/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet" />
 
     <!-- Icons css-->
-    <link href="http://localhost/appci4/public/assets/plugins/web-fonts/icons.css" rel="stylesheet" />
-    <link href="http://localhost/appci4/public/assets/plugins/web-fonts/font-awesome/font-awesome.min.css" rel="stylesheet" />
-    <link href="http://localhost/appci4/public/assets/plugins/web-fonts/plugin.css" rel="stylesheet" />
+    <link href="<?= base_url('/assets/plugins/web-fonts/icons.css') ?>" rel="stylesheet" />
+    <link href="<?= base_url('/assets/plugins/web-fonts/font-awesome/font-awesome.min.css') ?>" rel="stylesheet" />
+    <link href="<?= base_url('/assets/plugins/web-fonts/plugin.css') ?>" rel="stylesheet" />
 
     <!-- Style css-->
-    <link href="http://localhost/appci4/public/assets/css/style.css" rel="stylesheet" />
-    <link href="http://localhost/appci4/public/assets/css/skins.css" rel="stylesheet" />
-    <link href="http://localhost/appci4/public/assets/css/dark-style.css" rel="stylesheet" />
-    <link href="http://localhost/appci4/public/assets/css/colors/default.css" rel="stylesheet" />
+    <link href="<?= base_url('/assets/css/style.css') ?>" rel="stylesheet" />
+    <link href="<?= base_url('/assets/css/skins.css') ?>" rel="stylesheet" />
+    <link href="<?= base_url('/assets/css/dark-style.css') ?>" rel="stylesheet" />
+    <link href="<?= base_url('/assets/css/colors/default.css') ?>" rel="stylesheet" />
 
     <!-- Color css-->
-    <link id="theme" rel="stylesheet" type="text/css" media="all" href="http://localhost/appci4/public/assets/css/colors/color.css" />
+    <link id="theme" rel="stylesheet" type="text/css" media="all" href="<?= base_url('/assets/css/colors/color.css') ?>" />
 
     <!-- Select2 css-->
-    <link href="http://localhost/appci4/public/assets/plugins/select2/css/select2.min.css" rel="stylesheet">
+    <link href="<?= base_url('/assets/plugins/select2/css/select2.min.css') ?>" rel="stylesheet">
 
     <!-- Sidemenu css-->
-    <link href="http://localhost/appci4/public/assets/css/sidemenu/sidemenu.css" rel="stylesheet">
+    <link href="<?= base_url('/assets/css/sidemenu/sidemenu.css') ?>" rel="stylesheet">
 
     <!-- Mutipleselect css-->
-    <link rel="stylesheet" href="http://localhost/appci4/public/assets/plugins/multipleselect/multiple-select.css">
+    <link rel="stylesheet" href="<?= base_url('/assets/plugins/multipleselect/multiple-select.css') ?>">
 
 </head>
 
@@ -46,7 +46,7 @@
 
     <!-- Loader -->
     <div id="global-loader">
-        <img src="http://localhost/appci4/public/assets/img/loader.svg" class="loader-img" alt="Loader">
+        <img src="<?= base_url('/assets/img/loader.svg') ?>" class="loader-img" alt="Loader">
     </div>
     <!-- End Loader -->
 
@@ -61,7 +61,7 @@
                                 <h2 class="mt-0 text-white">B P K A D </h2>
 
                                 <div class="clearfix"></div>
-                                <img src="http://localhost/appci4/public/img/logo.png" class="ht-150 mb-0" alt="user">
+                                <img src="<?= base_url('/img/logo.png') ?>" class="ht-150 mb-0" alt="user">
                                 <h5 class="mt-0 text-white">Badan Pengelolaan Keuangan dan Aset Daerah Kota Medan</h5>
                                 <span class="tx-white-6 tx-13 mb-5 mt-xl-0"></span>
                             </div>
@@ -70,7 +70,7 @@
                             <div class="container-fluid">
                                 <div class="row row-sm">
                                     <div class="card-body mt-2 mb-2">
-                                        <img src="http://localhost/appci4/public/assets/img/brand/logo.png" class=" d-lg-none header-brand-img text-left float-left mb-4" alt="logo">
+                                        <img src="<?= base_url('/assets/img/brand/logo.png') ?>" class=" d-lg-none header-brand-img text-left float-left mb-4" alt="logo">
                                         <div class="clearfix"></div>
                                         <form action="/auth/login" method="post">
                                             <h5 class="text-left mb-2">Agenda Rapat</h5>
@@ -112,69 +112,69 @@
 
 </body>
 <!-- Jquery js-->
-<script src="http://localhost/appci4/public/assets/plugins/jquery/jquery.min.js"></script>
+<script src="<?= base_url('/assets/plugins/jquery/jquery.min.js') ?>"></script>
 
 <!-- Bootstrap js-->
-<script src="http://localhost/appci4/public/assets/plugins/bootstrap/js/popper.min.js"></script>
-<script src="http://localhost/appci4/public/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="<?= base_url('/assets/plugins/bootstrap/js/popper.min.js') ?>"></script>
+<script src="<?= base_url('/assets/plugins/bootstrap/js/bootstrap.min.js') ?>"></script>
 
 <!-- Perfect-scrollbar js -->
-<script src="http://localhost/appci4/public/assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+<script src="<?= base_url('/assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js') ?>"></script>
 
 <!-- Sidemenu js -->
-<script src="http://localhost/appci4/public/assets/plugins/sidemenu/sidemenu.js"></script>
+<script src="<?= base_url('/assets/plugins/sidemenu/sidemenu.js') ?>"></script>
 
 <!-- Sidebar js -->
-<script src="http://localhost/appci4/public/assets/plugins/sidebar/sidebar.js"></script>
+<script src="<?= base_url('/assets/plugins/sidebar/sidebar.js') ?>"></script>
 
 <!-- Select2 js-->
-<script src="http://localhost/appci4/public/assets/plugins/select2/js/select2.min.js"></script>
+<script src="<?= base_url('/assets/plugins/select2/js/select2.min.js') ?>"></script>
 
 <!-- Sticky js -->
-<script src="http://localhost/appci4/public/assets/js/sticky.js"></script>
+<script src="<?= base_url('/assets/js/sticky.js') ?>"></script>
 
 <!-- Custom js -->
-<script src="http://localhost/appci4/public/assets/js/custom.js"></script>
+<script src="<?= base_url('/assets/js/custom.js') ?>"></script>
 
 <!-- Internal Chart.Bundle js-->
-<script src="http://localhost/appci4/public/assets/plugins/chart.js/Chart.bundle.min.js"></script>
+<script src="<?= base_url('/assets/plugins/chart.js/Chart.bundle.min.js') ?>"></script>
 
 <!-- Peity js-->
-<script src="http://localhost/appci4/public/assets/plugins/peity/jquery.peity.min.js"></script>
+<script src="<?= base_url('/assets/plugins/peity/jquery.peity.min.js') ?>"></script>
 
 <!-- Internal Morris js -->
-<script src="http://localhost/appci4/public/assets/plugins/raphael/raphael.min.js"></script>
-<script src="http://localhost/appci4/public/assets/plugins/morris.js/morris.min.js"></script>
+<script src="<?= base_url('/assets/plugins/raphael/raphael.min.js') ?>"></script>
+<script src="<?= base_url('/assets/plugins/morris.js/morris.min.js') ?>"></script>
 
 <!-- Circle Progress js-->
-<script src="http://localhost/appci4/public/assets/js/circle-progress.min.js"></script>
-<script src="http://localhost/appci4/public/assets/js/chart-circle.js"></script>
+<script src="<?= base_url('/assets/js/circle-progress.min.js') ?>"></script>
+<script src="<?= base_url('/assets/js/chart-circle.js') ?>"></script>
 
 <!-- Internal Dashboard js-->
-<script src="http://localhost/appci4/public/assets/js/index.js"></script>
+<script src="<?= base_url('/assets/js/index.js') ?>"></script>
 
 <!-- Internal Jquery-Ui js-->
-<script src="http://localhost/appci4/public/assets/plugins/jquery-ui/ui/widgets/datepicker.js"></script>
+<script src="<?= base_url('/assets/plugins/jquery-ui/ui/widgets/datepicker.js') ?>"></script>
 
 <!-- Internal Jquery.maskedinput js-->
-<script src="http://localhost/appci4/public/assets/plugins/jquery.maskedinput/jquery.maskedinput.js"></script>
+<script src="<?= base_url('/assets/plugins/jquery.maskedinput/jquery.maskedinput.js') ?>"></script>
 
 <!-- Internal Specturm-colorpicker js-->
-<script src="http://localhost/appci4/public/assets/plugins/spectrum-colorpicker/spectrum.js"></script>
+<script src="<?= base_url('/assets/plugins/spectrum-colorpicker/spectrum.js') ?>"></script>
 
 <!-- Internal Ion-rangeslider js-->
-<script src="http://localhost/appci4/public/assets/plugins/ion-rangeslider/js/ion.rangeSlider.min.js"></script>
+<script src="<?= base_url('/assets/plugins/ion-rangeslider/js/ion.rangeSlider.min.js') ?>"></script>
 
 <!-- Select2 js-->
-<script src="http://localhost/appci4/public/assets/js/select2.js"></script>
+<script src="<?= base_url('/assets/js/select2.js') ?>"></script>
 
 <!--Bootstrap-datepicker js-->
-<script src="http://localhost/appci4/public/assets/plugins/bootstrap-datepicker/bootstrap-datepicker.js"></script>
+<script src="<?= base_url('/assets/plugins/bootstrap-datepicker/bootstrap-datepicker.js') ?>"></script>
 
 <!-- Internal jquery-simple-datetimepicker js -->
-<script src="http://localhost/appci4/public/assets/plugins/amazeui-datetimepicker/js/amazeui.datetimepicker.min.js"></script>
+<script src="<?= base_url('/assets/plugins/amazeui-datetimepicker/js/amazeui.datetimepicker.min.js') ?>"></script>
 
 <!-- Internal Form-elements js-->
-<script src="http://localhost/appci4/public/assets/js/form-elements.js"></script>
+<script src="<?= base_url('/assets/js/form-elements.js') ?>"></script>
 
 </html>
